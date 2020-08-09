ad73b82e9ca0f1a100dfea2300b92c13d3085735
Vincentfritsch <fritsch-vincent@hotmail.fr>
2020-08-09 09:53:47 +0200

    capitalizeFirstLetters function done

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,13 +1,15 @@
 const assert = require('assert');
 // WRITE THE ACTUAL FUNCTION HERE
 function capitalizeFirstLetters(str) {
-
+    return (str + '').replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+        return $1.toUpperCase();
+    });
 }
 //Assertions
 
 assert.strictEqual(typeof capitalizeFirstLetters,'function')
 assert.strictEqual(capitalizeFirstLetters.length, 1)
-assert.strictEqual(capitalizeFirstLetters('I am Grrot'), 'I Am Groot')
+assert.strictEqual(capitalizeFirstLetters('I am Groot'), 'I Am Groot')
 assert.strictEqual(capitalizeFirstLetters('i'), 'I')
 assert.strictEqual(capitalizeFirstLetters('I'), 'I')
 assert.strictEqual(capitalizeFirstLetters(''), '')
